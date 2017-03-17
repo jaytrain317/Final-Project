28b7a22048d36c47b420ac6bc26e260afb79dcde
Jeremy Lindsey <jaytrain317@yahoo.com>
2017-03-17 03:56:51 +0000

    Created New workout form

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,4 +2,25 @@ class WorkoutsController < ApplicationController
     def new
       @workouts = Workout.new
     end
+    
+    
+    def create 
+      @workouts = Workout.new(workout_params)
+      
+      if @workouts.save
+        flash[:notice] = "Workout was Successfully logged"
+        redirect_to workout_path(@workouts)
+      else
+        render 'new'
+      end
+    end
+    
+    def show
+      @workout = Workout.find(params[:id])
+    end
+    private
+    
+    def workout_params
+      params.require(:workouts).permit(:weight, :exercise, :repetition, :time)
+    end
 end
